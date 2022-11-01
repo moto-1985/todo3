3f4f792af0f49944b84d626565ccdfbe121c45f5
motoki arai <arai@nal.vn>
2022-11-01 13:27:49 +0900

    余計なルートコメントアウト

--- app/routes/web.php
+++ app/routes/web.php
@@ -18,9 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 })->name('top');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
